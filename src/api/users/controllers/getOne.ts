683c0f6a695f14471c3e3a4c5c6808e707bbd895
MatthiasWanner <matthias.wanner@gmail.com>
2021-07-21 19:07:40 +0200

    Get one user done

---
+++ src/api/users/controllers/getOne.ts
@@ -0,0 +1,31 @@
+import { prisma } from "../../../../prisma/prisma.client";
+import { UserHandlers } from "../../../../env";
+
+const getOne: UserHandlers["getOne"] = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+      select: {
+        id: true,
+        username: true,
+        firstname: true,
+        lastname: true,
+        email: true,
+        biography: true,
+        avatarUrl: true,
+        role: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+    if (!user) throw new Error("User not found");
+    return res.status(200).json(user);
+  } catch (e) {
+    next(e);
+  }
+};
+
+export default getOne;
